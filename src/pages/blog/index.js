045d085c9307b2949de4a06f9596ab918bf93e83
Hemlata Bhundhoo <hemlata.b@hotmail.co.uk>
2019-09-01 13:49:28 +0400

    Update events section and background image

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -10,14 +10,14 @@ export default class BlogIndexPage extends React.Component {
         <div
           className="full-width-image-container margin-top-0"
           style={{
-            backgroundImage: `url('/img/blog-index.jpg')`,
+            backgroundImage: `url('/img/events.jpg')`,
           }}
         >
           <h1
             className="has-text-weight-bold is-size-1"
             style={{
               boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#f40',
+              backgroundColor: 'rgb(0,191,255)',
               color: 'white',
               padding: '1rem',
             }}
